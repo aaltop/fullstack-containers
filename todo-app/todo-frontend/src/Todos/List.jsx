614b6bb362f2f0d3ee3a58862202ff92efc886db
TA <aaltopi@gmail.com>
2025-01-16 15:24:41 +0200

    Add partial "Testing during the build process", create separate Todo component (12.14)

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Todo from './Todo'
+
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   const onClickDelete = (todo) => () => {
     deleteTodo(todo)
@@ -11,36 +13,12 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
 
   return (
     <>
-      {todos.map(todo => {
-        const doneInfo = (
-          <>
-            <span>This todo is done</span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-            </span>
-          </>
-        )
-
-        const notDoneInfo = (
-          <>
-            <span>
-              This todo is not done
-            </span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-              <button onClick={onClickComplete(todo)}> Set as done </button>
-            </span>
-          </>
-        )
-
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-            <span>
-              {todo.text} 
-            </span>
-            {todo.done ? doneInfo : notDoneInfo}
-          </div>
-        )
+      {todos.map( (todo, index) => {
+        return <Todo
+          todo={todo}
+          onDelete={onClickDelete}
+          onComplete={onClickComplete}
+        />
       }).reduce((acc, cur) => [...acc, <hr />, cur], [])}
     </>
   )
---
+++ todo-app/todo-frontend/src/Todos/Todo.jsx
@@ -0,0 +1,35 @@
+
+
+
+export default function Todo({ todo, onDelete, onComplete})
+{
+    const doneInfo = (
+        <>
+          <span>This todo is done</span>
+          <span>
+            <button onClick={onDelete(todo)}> Delete </button>
+          </span>
+        </>
+      )
+
+      const notDoneInfo = (
+        <>
+          <span>
+            This todo is not done
+          </span>
+          <span>
+            <button onClick={onDelete(todo)}> Delete </button>
+            <button onClick={onComplete(todo)}> Set as done </button>
+          </span>
+        </>
+      )
+
+      return (
+        <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
+          <span>
+            {todo.text} 
+          </span>
+          {todo.done ? doneInfo : notDoneInfo}
+        </div>
+      )
+}
