01cf3ef61f81de15a7fedcc1f663796a0c287824
TA <aaltopi@gmail.com>
2025-01-16 20:50:16 +0200

    Add "Set up a frontend development environment" (12.15)
    Turns out the local volume can be shared just fine on Windows too,
    though there is a slight hickup which makes Vite unable to fast refresh,
    but that can be fixed with enabling polling on the server (see vite config).
    Could also supposedly do development entirely in WSL to fix the issue,
    but where's the fun in that.
    
    Secondly, I'm only sharing the src file as that's mostly what's
    needed for fast refresh. Also, sharing the whole folder is generally
    the issue with the whole "missing this-and-that" in node_modules: the install
    should install the platform-specific dependencies, but the volume share
    causes everything to be copied over -- as I understand it anyway -- and
    effectively overwrites whatever the install did, IF node_modules is
    present in the shared host folder as well.

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( ({command}) => ({
   plugins: [react()],
   test: {
     environment: "jsdom"
+  },
+  // see https://github.com/vitejs/vite/issues/1153#issuecomment-785467271
+  // and specifically https://vite.dev/config/server-options.html#server-watch
+  // Needed when working on Windows and WSL2 with the code of the Windows
+  // side and trying to mount stuff from the host to the container
+  // Vite doesn't see the updates, but the contents do still update
+  // verifiably in the container
+  server: {
+    watch: {
+      usePolling: command === "serve" ? true : false
+    }
   }
-})
+}))
